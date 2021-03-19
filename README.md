82302132c71ca0fbe661bb6a0c99e683ca100f61
Gaurav Kumar <gauravk26800@gmail.com>
2021-03-20 02:10:26 +0530

    completed freecodecamp basic node and express

--- README.md
+++
@@ -1,1 +0,0 @@
-# [Introduction to the Basic Node and Express Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/)
--- boilerplate-express-gomix/myApp.js
+++ boilerplate-express-gomix/myApp.js
@@ -1,37 +1,49 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(function(req, res, next){
+  console.log(req.method + " " + req.path + " - " + req.ip)
+  next()
+})
+app.use('/public', express.static(__dirname+'/public'));
+app.use(bodyParser.urlencoded({extended:false}));
 
+console.log("Hello World");
 
+app.get('/', (req, res)=>{
+  // res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
+});
 
+app.get('/json', (req, res)=>{
+  if(process.env.MESSAGE_STYLE === 'uppercase'){
+    res.json({"message": "HELLO JSON"});
+  }
+  else{
+    res.json({"message": "Hello json"});  
+  }
+});
 
+app.get('/now', function(req, res, next){
+  req.time = new Date().toString();
+  next();
+}, function(req, res){
+  res.json({"time": req.time});
+});
 
+app.get('/:word/echo', (req, res)=>{
+  res.json({"echo":req.params.word});
+});
 
+app.get('/name', (req, res)=>{
+  res.json({"name": req.query.first + " " + req.query.last});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.post('/name', (req, res)=>{
+  var string = req.body.first + " " + req.body.last;
+  res.json({name: string});
+});
 
 
 
